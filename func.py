8870cf015300adfbb3b85eca7a8ed675e98f5bab
m&m <324585@gmail.com>
2017-11-05 01:30:15 -0300

    Update func.py
    Agregadas las funciones correspondientes a los nuevos botones.

--- func.py
+++ func.py
@@ -1,3 +1,13 @@
 def boton_comenzar():
     return [False, False, True]
 
+
+def boton_pausa():
+    return [False, True, False]
+
+
+def boton_despausa():
+    return [False, False, True]
+
+def boton_menu():
+    return [True, False, False]
