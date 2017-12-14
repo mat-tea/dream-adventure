407ef7356a7f5c70fc9ab2051e5f739e5c661ae9
m&m <324585@gmail.com>
2017-12-13 22:13:35 -0300

    Update func.py
    agregue funciones necesarias para el menu de resoluciones

--- func.py
+++ func.py
@@ -1,11 +1,35 @@
+def change_res(width, height):
+    global screen_sz
+    screen_sz = width, height
+
+
 def boton_comenzar():
     return 2
+
 
 def boton_pausa():
     return 1
 
+
 def boton_despausa():
     return 2
 
+
 def boton_menu():
     return 0
+
+
+def boton_43():
+    return 0
+
+
+def boton_169():
+    return 1
+
+
+def boton_1610():
+    return 2
+
+
+def cambio_de_resolucion():
+    return "end"
